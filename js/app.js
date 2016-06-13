0f652965269939195b94888f2835f68d7135600a
Vladislav <v.kirbaba@gmail.com>
2016-06-13 15:47:44 +0300

    Операции

---
+++ js/app.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+  var et_pb_custom = {
+      "ajaxurl": "http:\/\/www.ucontrol.de\/ajax",
+      "images_uri": "http:\/\/www.ucontrol.de\/skin_main\/images",
+      "builder_images_uri": "http:\/\/www.ucontrol.de\/skin_main\/includes\/builder\/images",
+      "et_load_nonce": "d26180c4e9",
+      "subscription_failed": "Please, check the fields below to make sure you entered the correct information.",
+      "fill_message": "Please, fill in the following fields:",
+      "contact_error_message": "Please, fix the following errors:",
+      "invalid": "Invalid email",
+      "captcha": "Captcha",
+      "prev": "Prev",
+      "previous": "Vorherige",
+      "next": "Nächste",
+      "wrong_captcha": "You entered the wrong number in captcha.",
+      "is_builder_plugin_used": "",
+      "is_divi_theme_used": "1",
+      "widget_search_selector": ".widget_search"
+  };
+      </script>
+      <script type="text/javascript" src="js/frontend-builder-scripts.js"></script>
+      <script type="text/javascript" src="js/wp-embed.js"></script>
+      <script type="text/javascript">
+  var icl_vars = {
+      "current_language": "de",
+      "icl_home": "http:\/\/www.ucontrol.de\/",
+      "ajax_url": "http:\/\/www.ucontrol.de\/ajax",
+      "url_type": "2"
+  };
+
+  var ninja_forms_settings = {
+      "ajax_msg_format": "inline",
+      "password_mismatch": "The passwords provided do not match.",
+      "plugin_url": "http:\/\/www.ucontrol.de\/inc\/plugins\/ninja-forms\/",
+      "datepicker_args": {
+          "dateFormat": "dd.mm.yy"
+      },
+      "currency_symbol": "$",
+      "date_format": "dd.mm.yy"
+  };
+  var thousandsSeparator = ".";
+  var decimalPoint = ",";
+  var ninja_forms_form_1_settings = {
+      "ajax": "0",
+      "hide_complete": "1",
+      "clear_complete": "1"
+  };
+  var ninja_forms_form_1_calc_settings = {
+      "calc_value": {
+          "12": {
+              "Frau": 0,
+              "Herr": 0,
+              "Firma": 0
+          }
+      },
+      "calc_fields": []
+  };
+  var ninja_forms_password_strength = {
+      "empty": "Stärkeindikator",
+      "short": "Sehr schwach",
+      "bad": "Schwach",
+      "good": "Mittel",
+      "strong": "Stark",
+      "mismatch": "Keine Übereinstimmung"
+  };
+});
